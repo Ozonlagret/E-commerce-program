8bd9c12eccada67c021ef517be726208f53f0e8c
Ozonlagret <tim.drejhede@gmail.com>
2025-04-16 10:40:08 +0200

    Put queries in a class

--- E-commerce program/Program.cs
+++ E-commerce program/Program.cs
@@ -37,113 +37,23 @@ namespace E_commerce_program
                         break;
 
                     case 2: // Lista alla leverantörer som har produkter med ett lagersaldo under 10 enheter
-                        var suppliers =
-                            from s in 
-                                (from s in context.Suppliers
-                                join p in context.Products on s.SupplierId equals p.SupplierId
-                                where p.StockQuantity < 10
-                                select s)
-                            group s by s.SupplierId into g
-                            select g.First();
-
-                        foreach (Supplier supplier in suppliers)
-                        {
-                            Console.WriteLine(supplier.Name);
-                        }
-
-                        Console.ReadLine();
+                        Queries.ListSuppliers(context);
                         break;
 
                     case 3: // Beräkna det totala ordervärdet för alla ordrar gjorda under den senaste månaden
-                        DateTime monthEarlier = DateTime.Today.AddMonths(-1);
-                        DateTime today = DateTime.Today;
-                        var orderValue =
-                            (from o in context.Orders
-                             where o.OrderDate >= monthEarlier && o.OrderDate <= today
-                             select o.TotalAmount).Sum();
-
-                        Console.WriteLine("Totala ordervärdet: " + orderValue);
-                        Console.ReadLine();
+                        Queries.CalculateOrderValue(context);
                         break;
 
-
                     case 4: // Hitta de 3 mest sålda produkterna baserat på OrderDetail-data
-                        var topProducts =
-                            (from p in context.Products
-                            join od in context.OrderDetails on p.ProductId equals od.ProductId
-                            group od by p.ProductId into g
-                            orderby g.Sum(item => item.Quantity) descending
-                            select new
-                            {
-                                 Product = g.First().Product.Name,
-                                 QuantitySold = g.Sum(item => item.Quantity)
-                            }).Take(3);
-
-                        foreach (var product in topProducts)
-                        {
-                            Console.WriteLine($"{product.Product} | Antal sålda: {product.QuantitySold}");
-                        }
-
-                        Console.ReadLine();
+                        Queries.FindMostSoldProducts(context);
                         break;
 
                     case 5: // Lista alla kategorier och antalet produkter i varje kategori
-                        var categoriesProductCount =
-                            from p in context.Products
-                            join c in context.Categories on p.CategoryId equals c.CategoryId
-                            group p by c.CategoryId into g
-                            select new
-                            {
-                                Category = g.First().Category.Name,
-                                NumberOfProducts = g.Count()
-                            };
-
-                        foreach (var category in categoriesProductCount)
-                        {
-                            Console.WriteLine(category.Category + " | " + category.NumberOfProducts);
-                        }
-
-                        Console.ReadLine();
+                        Queries.CategoriesContainedProducts(context);
                         break;
 
                     case 6: // Hämta alla ordrar med tillhörande kunduppgifter och orderdetaljer där totalbeloppet överstiger 1000 kr
-                        var result =
-                        from o in context.Orders
-                        where o.TotalAmount > 1000
-                        join c in context.Customers on o.CustomerId equals c.CustomerId
-                        join od in context.OrderDetails on o.OrderId equals od.OrderId
-                        join p in context.Products on od.ProductId equals p.ProductId
-                        select new
-                        {
-                            o.OrderId,
-                            o.OrderDate,
-                            o.TotalAmount,
-                            CustomerName = c.Name,
-                            p.Name,
-                            od.Quantity,
-                            od.UnitPrice
-                        };
-
-                        var groups = result
-                            .GroupBy(r => r.OrderId);
-
-                        Console.WriteLine($"{"Order-ID", -9}  {"Datum", -11}  {"Kund", -21}  {"Totalvärde", -11}  {"Produktnamn", - 21}" +
-                            $"  {"Kvantitet", -11}  {"Enhetspris"}");
-
-                        foreach (var group in groups)
-                        {
-                            string formattedDate = group.First().OrderDate.ToString("yyyy-MM-dd");
-                            Console.Write($"{group.Key, -8} | {formattedDate, -10} | {group.First().CustomerName,-20} |" +
-                                $" {group.First().TotalAmount,-10} | ");
-
-                            foreach (var item in group)
-                            {
-                                Console.SetCursorPosition(60, Console.CursorTop);
-                                Console.WriteLine($"{ item.Name,-20} | { item.Quantity,-10} | { item.UnitPrice}");
-                            }
-                        }
-
-                        Console.ReadLine();
+                        Queries.GetOrderAndCustomerData(context);
                         break;
 
                     default:
--- E-commerce program/Queries.cs
+++ E-commerce program/Queries.cs
@@ -23,5 +23,119 @@ namespace E_commerce_program
                 Console.WriteLine(product.Name + " | " + product.Price);
             }
         }
+
+        public static void ListSuppliers(ECommerceProgramContext context)
+        {
+            var suppliers =
+                            from s in
+                                (from s in context.Suppliers
+                                 join p in context.Products on s.SupplierId equals p.SupplierId
+                                 where p.StockQuantity < 10
+                                 select s)
+                            group s by s.SupplierId into g
+                            select g.First();
+
+            foreach (Supplier supplier in suppliers)
+            {
+                Console.WriteLine(supplier.Name);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void CalculateOrderValue(ECommerceProgramContext context)
+        {
+            DateTime monthEarlier = DateTime.Today.AddMonths(-1);
+            DateTime today = DateTime.Today;
+            var orderValue =
+                (from o in context.Orders
+                 where o.OrderDate >= monthEarlier && o.OrderDate <= today
+                 select o.TotalAmount).Sum();
+
+            Console.WriteLine("Totala ordervärdet: " + orderValue);
+            Console.ReadLine();
+        }
+
+        public static void FindMostSoldProducts(ECommerceProgramContext context)
+        {
+            var topProducts =
+                            (from p in context.Products
+                             join od in context.OrderDetails on p.ProductId equals od.ProductId
+                             group od by p.ProductId into g
+                             orderby g.Sum(item => item.Quantity) descending
+                             select new
+                             {
+                                 Product = g.First().Product.Name,
+                                 QuantitySold = g.Sum(item => item.Quantity)
+                             }).Take(3);
+
+            foreach (var product in topProducts)
+            {
+                Console.WriteLine($"{product.Product} | Antal sålda: {product.QuantitySold}");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void CategoriesContainedProducts(ECommerceProgramContext context)
+        {
+            var categoriesProductCount =
+                            from p in context.Products
+                            join c in context.Categories on p.CategoryId equals c.CategoryId
+                            group p by c.CategoryId into g
+                            select new
+                            {
+                                Category = g.First().Category.Name,
+                                NumberOfProducts = g.Count()
+                            };
+
+            foreach (var category in categoriesProductCount)
+            {
+                Console.WriteLine(category.Category + " | " + category.NumberOfProducts);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void GetOrderAndCustomerData(ECommerceProgramContext context)
+        {
+            var result =
+                        from o in context.Orders
+                        where o.TotalAmount > 1000
+                        join c in context.Customers on o.CustomerId equals c.CustomerId
+                        join od in context.OrderDetails on o.OrderId equals od.OrderId
+                        join p in context.Products on od.ProductId equals p.ProductId
+                        select new
+                        {
+                            o.OrderId,
+                            o.OrderDate,
+                            o.TotalAmount,
+                            CustomerName = c.Name,
+                            p.Name,
+                            od.Quantity,
+                            od.UnitPrice
+                        };
+
+            var groups = result
+                .GroupBy(r => r.OrderId);
+
+            Console.WriteLine($"{"Order-ID",-9}  {"Datum",-11}  {"Kund",-21}  {"Totalvärde",-11}  {"Produktnamn",-21}" +
+                $"  {"Kvantitet",-11}  {"Enhetspris"}");
+
+            foreach (var group in groups)
+            {
+                string formattedDate = group.First().OrderDate.ToString("yyyy-MM-dd");
+                Console.Write($"{group.Key,-8} | {formattedDate,-10} | {group.First().CustomerName,-20} |" +
+                    $" {group.First().TotalAmount,-10} | ");
+
+                foreach (var item in group)
+                {
+                    Console.SetCursorPosition(60, Console.CursorTop);
+                    Console.WriteLine($"{item.Name,-20} | {item.Quantity,-10} | {item.UnitPrice}");
+                }
+            }
+
+            Console.ReadLine();
+        }
     }
 }
